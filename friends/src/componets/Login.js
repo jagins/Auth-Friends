19a91ee49bc21413f50c9a5b3de5067a00f542b0
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-01-28 15:00:55 -0600

    login form created and console logged username and password works

--- friends/src/componets/Login.js
+++ friends/src/componets/Login.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 function Login()
 {
-    return (
-        <div>
+    const [credentials, setCredentials] = useState({username: '', password: ''});
 
+    const handleChange = e =>
+    {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value});
+    }
+
+    const login = e =>
+    {
+        e.preventDefault();
+    }
+    return (
+        <div className='login-form'>
+            <form onSubmit={login}>
+                <input 
+                    type='text'
+                    name='username'
+                    value={credentials.username}
+                    onChange={handleChange}/>
+                <input
+                    type='password'
+                    name='password'
+                    value={credentials.password}
+                    onChange={handleChange}/>
+                <button>Log in</button>
+            </form>
         </div>
     );
 }
